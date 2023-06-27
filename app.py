a7357ae301a6bb28d82a12f72802c38258b11760
maulanayos <87421880+maulanayos@users.noreply.github.com>
2023-06-27 04:39:54 +0000

    added bottom content

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', tesdynamic='Peta')
+
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
